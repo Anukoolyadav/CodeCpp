8d6cdd29ee3d643c91f1711d04bbb6a3de4cf6b5
Anukool Yadav <anukoolyadav@Anukools-MacBook-Air.local>
2025-05-19 17:28:40 +0530

    Update I.cpp with latest edits

--- I.cpp
+++ I.cpp
@@ -1,24 +1,57 @@
-// #include<stdc++.h>
 #include<iostream>
+#include<vector>
 using namespace std;
+
+void DisplayM(const vector<vector<int>>& arr, int n){
+    cout << "printing the matrix" << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+vector<vector<int>> RowTransfer(const vector<vector<int>>& arr, int n , int x, int y){
+    cout<<"Row transforming "<<endl;
+    vector<vector<int>> arr1(n, vector<int>(n));
+    arr1=arr;
+    for(int j=0;j<n;j++){
+            arr1[x-1][j]=arr[y-1][j];
+        }
+    for(int j=0;j<n;j++){
+            arr1[y-1][j]=arr[x-1][j];
+        }
+    return arr1;
+
+}
+vector<vector<int>> ColumnTransfer(const vector<vector<int>>& arr, int n , int x, int y){
+    cout<<"column transforming "<<endl;
+    vector<vector<int>> arr1(n, vector<int>(n));
+    arr1=arr;
+    for(int j=0;j<n;j++){
+            arr1[j][x-1]=arr[j][y-1];
+        }
+    for(int j=0;j<n;j++){
+            arr1[j][y-1]=arr[j][x-1];
+        }
+    return arr1;
+
+}
+
 int main(){
-    int n,x,y;
-    cin>>n>>x>>y;
-    int arr[n][n];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>arr[i][j];
+    int n, x, y;
+    cin >> n >> x >> y;
+    vector<vector<int>> arr(n, vector<int>(n));
+    vector<vector<int>> arr1(n, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
         }
     }
-    cout<<"printing the matrix"<<endl;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<"he";
-    
-    
-    
+    DisplayM(arr, n);
+    arr1=RowTransfer(arr,n,x,y);
+   
+    DisplayM(arr1,n);
+    arr1=ColumnTransfer(arr1,n,x,y);
+    DisplayM(arr1,n);
 }
